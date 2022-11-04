29c1b7847bc146bc069252da62060e86fc3c94b3
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-11-04 19:32:21 +0700

    Add files via upload

---
+++ LoopFor/src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 999;
+		for (int i = a; i < b; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i + " ");
+				
+			}
+		}
+	}
+
+}
